af0f87850530dbd60babd218895d51c29b6402b2
truongtin2003 <106795994+truongtin2003@users.noreply.github.com>
2022-06-22 07:37:47 +0700

    update

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -10,23 +10,35 @@ const list_btn = document.getElementById("menu-button");
 const list_content = document.querySelector('.nav-list-mobile');
 const modal = document.querySelector('.modal');
 
-search_btn.addEventListener('click', () => {
-    if (nonsearch.style.display == 'none') {
-        nonsearch.style.display = 'block';
-        nonsearch2.style.display = 'block';
-        nonsearch3.style.display = 'block';
-        nonsearch4.style.display = 'block';
-        nonsearch5.style.display = 'block';
-        search_bar.style.display = 'none';
-    } else {
-        nonsearch.style.display = 'none';
-        nonsearch2.style.display = 'none';
-        nonsearch3.style.display = 'none';
-        nonsearch4.style.display = 'none';
-        nonsearch5.style.display = 'none';
-        search_bar.style.display = 'block';
+const mq = window.matchMedia("(min-width: 700px)")
+mq.addListener(() =>{
+    if (mq.matches) {
+        list_btn.classList.remove('is-opened');
+        modal.style.display = 'none';
+        list_content.classList.add('growUp');
+        setTimeout(() => {
+            list_content.style.display = 'none';
+        }, 200);
     }
 })
+
+// search_btn.addEventListener('click', () => {
+//     if (nonsearch.style.display == 'none') {
+//         nonsearch.style.display = 'block';
+//         nonsearch2.style.display = 'block';
+//         nonsearch3.style.display = 'block';
+//         nonsearch4.style.display = 'block';
+//         nonsearch5.style.display = 'block';
+//         search_bar.style.display = 'none';
+//     } else {
+//         nonsearch.style.display = 'none';
+//         nonsearch2.style.display = 'none';
+//         nonsearch3.style.display = 'none';
+//         nonsearch4.style.display = 'none';
+//         nonsearch5.style.display = 'none';
+//         search_bar.style.display = 'block';
+//     }
+// })
 
 list_btn.addEventListener('click', () => {
     if (list_btn.classList.contains('is-opened')) {
